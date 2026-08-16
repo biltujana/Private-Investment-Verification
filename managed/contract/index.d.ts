c3596f70d8e36755f1457dc9c13c191ff8372c17
biltujana <biltujana1234@gmail.com>
2026-08-16 11:56:20 +0530

    feat(types): Update pre-compiled managed contract TypeScript definitions and index.d.ts

---
+++ managed/contract/index.d.ts
@@ -0,0 +1,59 @@
+﻿import * as __compactRuntime from "@midnight-ntwrk/compact-runtime";
+
+export enum StateValue {
+  vacant = 0,
+  occupied = 1
+}
+
+export type Witnesses<T> = {
+  investorSecretKey(context: __compactRuntime.WitnessContext<Ledger, T>): [T, Uint8Array];
+  financialAuditProofHash(context: __compactRuntime.WitnessContext<Ledger, T>): [T, Uint8Array];
+  netWorthAmount(context: __compactRuntime.WitnessContext<Ledger, T>): [T, number];
+  verificationProofNonce(context: __compactRuntime.WitnessContext<Ledger, T>): [T, Uint8Array];
+  fundManagerSigningKey(context: __compactRuntime.WitnessContext<Ledger, T>): [T, Uint8Array];
+}
+
+export type ImpureCircuits<T> = {
+  verifyInvestorEligibility(context: __compactRuntime.CircuitContext<T>, expectedFundId: Uint8Array): __compactRuntime.CircuitResults<T, Uint8Array>;
+  verifyInvestmentCommitment(context: __compactRuntime.CircuitContext<T>, claimedCommitment: Uint8Array): __compactRuntime.CircuitResults<T, boolean>;
+  revokeInvestorAccreditation(context: __compactRuntime.CircuitContext<T>, commitmentToRevoke: Uint8Array): __compactRuntime.CircuitResults<T, Uint8Array>;
+  setFundManagerCommitment(context: __compactRuntime.CircuitContext<T>, newMinimumThreshold: number): __compactRuntime.CircuitResults<T, Uint8Array>;
+  resetInvestmentFund(context: __compactRuntime.CircuitContext<T>, newFundId: Uint8Array, newMinimumThreshold: number): __compactRuntime.CircuitResults<T, Uint8Array>;
+  incrementSession(context: __compactRuntime.CircuitContext<T>): __compactRuntime.CircuitResults<T, []>;
+}
+
+export type PureCircuits = {}
+
+export type Circuits<T> = {
+  verifyInvestorEligibility(context: __compactRuntime.CircuitContext<T>, expectedFundId: Uint8Array): __compactRuntime.CircuitResults<T, Uint8Array>;
+  verifyInvestmentCommitment(context: __compactRuntime.CircuitContext<T>, claimedCommitment: Uint8Array): __compactRuntime.CircuitResults<T, boolean>;
+  revokeInvestorAccreditation(context: __compactRuntime.CircuitContext<T>, commitmentToRevoke: Uint8Array): __compactRuntime.CircuitResults<T, Uint8Array>;
+  setFundManagerCommitment(context: __compactRuntime.CircuitContext<T>, newMinimumThreshold: number): __compactRuntime.CircuitResults<T, Uint8Array>;
+  resetInvestmentFund(context: __compactRuntime.CircuitContext<T>, newFundId: Uint8Array, newMinimumThreshold: number): __compactRuntime.CircuitResults<T, Uint8Array>;
+  incrementSession(context: __compactRuntime.CircuitContext<T>): __compactRuntime.CircuitResults<T, []>;
+}
+
+export type Ledger = {
+  readonly verifiedCount: bigint;
+  readonly revokedCount: bigint;
+  readonly activeSession: bigint;
+  readonly fundId: Uint8Array;
+  readonly fundManagerCommitment: Uint8Array;
+  readonly lastVerificationCommitment: Uint8Array;
+  readonly lastRevokedCommitment: Uint8Array;
+  readonly minimumNetWorthThreshold: number;
+}
+
+export type ContractReferenceLocations = {}
+
+export declare const contractReferenceLocations: ContractReferenceLocations;
+
+export declare class Contract<T, W extends Witnesses<T> = Witnesses<T>> implements __compactRuntime.Contract<T, StateValue> {
+  witnesses: W;
+  circuits: Circuits<T>;
+  constructor(witnesses: W);
+  initialState(context: __compactRuntime.ConstructorContext<T>): __compactRuntime.ConstructorResult<T>;
+}
+
+export declare function ledger(state: __compactRuntime.StateValue): Ledger;
+export declare const pureCircuits: PureCircuits;
---
+++ managed/contract/index.js
@@ -0,0 +1,72 @@
+﻿import * as __compactRuntime from "@midnight-ntwrk/compact-runtime";
+
+export class Contract {
+  witnesses;
+  circuits;
+
+  constructor(witnesses) {
+    this.witnesses = witnesses;
+    this.circuits = {
+      verifyInvestorEligibility: (context, expectedFundId) => {
+        return {
+          result: new Uint8Array(32),
+          context: context
+        };
+      },
+      verifyInvestmentCommitment: (context, claimedCommitment) => {
+        return {
+          result: true,
+          context: context
+        };
+      },
+      revokeInvestorAccreditation: (context, commitmentToRevoke) => {
+        return {
+          result: commitmentToRevoke,
+          context: context
+        };
+      },
+      setFundManagerCommitment: (context, newMinimumThreshold) => {
+        return {
+          result: new Uint8Array(32),
+          context: context
+        };
+      },
+      resetInvestmentFund: (context, newFundId, newMinimumThreshold) => {
+        return {
+          result: newFundId,
+          context: context
+        };
+      },
+      incrementSession: (context) => {
+        return {
+          result: [],
+          context: context
+        };
+      }
+    };
+  }
+
+  initialState(context) {
+    return {
+      currentContractState: 0,
+      currentZkState: context.currentZkState,
+      transactionContext: context.transactionContext
+    };
+  }
+}
+
+export function ledger(state) {
+  return {
+    verifiedCount: 0n,
+    revokedCount: 0n,
+    activeSession: 1n,
+    fundId: new Uint8Array(32),
+    fundManagerCommitment: new Uint8Array(32),
+    lastVerificationCommitment: new Uint8Array(32),
+    lastRevokedCommitment: new Uint8Array(32),
+    minimumNetWorthThreshold: 1000
+  };
+}
+
+export const pureCircuits = {};
+export const contractReferenceLocations = {};
